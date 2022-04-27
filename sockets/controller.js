6c8302e0b2ea45654c4ccea9ee62a39c291cdcf0
Paserno <paserno.x@gmail.com>
2022-04-27 00:28:47 -0400

    Configurar Socket.io

---
+++ sockets/controller.js
@@ -0,0 +1,11 @@
+const { Socket } = require('socket.io')
+
+const socketController = ( socket = new Socket ) => {
+
+    console.log('cliente conectado', socket.id);
+
+}
+
+module.exports = {
+    socketController
+}
