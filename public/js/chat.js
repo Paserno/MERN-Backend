bcb18056560450cf111d0fa6cbe7f56d2489081c
Paserno <paserno.x@gmail.com>
2022-04-28 20:50:01 -0400

    Validat Token - Frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,36 @@
 
+const url = ( window.location.hostname.includes('localhost') )
+? 'http://localhost:8082/api/auth/'
+: '';
+let usuario = null;
+let socket  = null;
 
+// Validar el token de LocalStorage
+const validarJWT = async() => {
 
-const socket = io();
+    const tokens =  localStorage.getItem('token') || '';
+
+    if ( tokens.length <= 10 ){
+        window.location = 'index.html';
+        throw new Error('No hay token en el servidor');
+    }
+
+    const resp = await fetch( url, {
+        headers: { 'x-token': tokens }
+    });
+
+    const { usuario: userDB, token:  tokenDB } = await resp.json();
+    localStorage.setItem('token', tokenDB);
+    usuario = userDB;
+
+}
+
+const main = async() => {
+
+    // Validar JWT
+    await validarJWT();
+
+}
+
+main();
+// const socket = io();
